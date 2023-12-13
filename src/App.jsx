f338ce88a0c2050f9227bd7a060326ac4e5a15c0
Bruno <brunoamagar@campus.monlau.com>
2023-12-13 19:11:04 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,23 @@ function App() {
                     <a href=""><li>TICKETS</li></a>
                     <a href=""><li>GALLERY</li></a>
                     <a href=""><li>CONTACT</li></a>
+                    <a href=""><img id='login-img' src="https://img.icons8.com/ios-glyphs/30/user--v1.png" alt="user-log"/></a>
                 </nav>
             </header>
             <div className="Video">
                 <img src="https://shorturl.at/boIN1" alt="video_motion" />
+                <h1>3X3 INTERN <strong>BARCELONA</strong></h1>
             </div>
-            <Card />
-            <Card />
-            <Card />
+            <div className="container">
+                <Card title="About" text="Take a look to our mission with this project and
+                also spectators need to consider follow some rules and policies to create a 
+                respectfull environment."/>
+                <Card title="Tickets" text="Get your tickets now and connect with the 
+                street basketball family watching the best matches, meeting people and 
+                having fun with a lot of activities!"/>
+                <Card title="" text=""/>
+                <Card title="" text=""/>
+            </div>
         </div>
     )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import "./Card.css"
 
-function Card() {
+function Card(props) {
     return (
-        <div className='Card'>
-            <img width="100" height="100" src="/src/assets/tickets.png" alt="two-tickets"/>
-            <div className="elements">
-                <h2>Title</h2>
-                <p>text</p>
+            <div className='Card'>
+                <img src="/src/assets/tickets.png" alt="two-tickets" />
+                <div className="elements">
+                    <h2 id='tit'>{props.title}</h2>
+                    <p>{props.text}</p>
+                </div>
             </div>
-        </div>
     )
 }
 
