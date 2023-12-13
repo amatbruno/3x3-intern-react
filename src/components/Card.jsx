a124094cc21e0f1135f2268c373ae4346dc25bec
Bruno <brunoamagar@campus.monlau.com>
2023-12-13 17:32:31 +0100

    .

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Card.css"
+
+function Card() {
+    return (
+        <div className='Card'>
+            <img width="100" height="100" src="/src/assets/" alt="two-tickets"/>
+            <div className="elements">
+                <h2>Title</h2>
+                <p>text</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Homepage from './Homepage.jsx'
 import './index.css'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <Homepage />
+    <App />
   </>,
 )
