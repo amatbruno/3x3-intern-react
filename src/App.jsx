d1e86abed841c0acfe7e2073c1c4f95930d0d375
Bruno <brunoamagar@campus.monlau.com>
2023-12-13 20:10:20 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Card from './components/Card';
 
+
 function App() {
     return (
         <div className="App">
@@ -11,21 +12,23 @@ function App() {
                     <a href=""><li>TICKETS</li></a>
                     <a href=""><li>GALLERY</li></a>
                     <a href=""><li>CONTACT</li></a>
-                    <a href=""><img id='login-img' src="https://img.icons8.com/ios-glyphs/30/user--v1.png" alt="user-log"/></a>
+                    <a href=""><img id='login-img' src="/src/assets/login.png" alt="user-log"/></a>
                 </nav>
             </header>
             <div className="Video">
                 <img src="https://shorturl.at/boIN1" alt="video_motion" />
                 <h1>3X3 INTERN <strong>BARCELONA</strong></h1>
             </div>
+            <hr />
             <div className="container">
-                <Card title="About" text="Take a look to our mission with this project and
+                <Card title="ABOUT" text="Take a look to our mission with this competition and
                 also spectators need to consider follow some rules and policies to create a 
                 respectfull environment."/>
-                <Card title="Tickets" text="Get your tickets now and connect with the 
+                <Card title="TICKETS" text="Get your tickets now and connect with the 
                 street basketball family watching the best matches, meeting people and 
                 having fun with a lot of activities!"/>
-                <Card title="" text=""/>
+                <Card title="GALLERY" text="See the latest winners from older editions and the
+                chronicles of every final and semifinal"/>
                 <Card title="" text=""/>
             </div>
         </div>
