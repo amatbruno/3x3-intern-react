a12f51aa96346c998407e4cc8fbc2708b0389867
Bruno <brunoamagar@campus.monlau.com>
2023-12-15 19:27:30 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css';
+import React from 'react';
+import Typed from 'typed.js';
 import Card from './components/Card';
 import Video from './components/Video';
 import Header from './components/Header';
@@ -6,28 +8,47 @@ import Footer from './components/Footer';
 
 
 function App() {
+    const el = React.useRef(null);
+
+    React.useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ['Discover <strong>all of our services.</strong>',"Discover <strong>new experiencies.</strong>", 
+            "Discover <strong>pure basketball.</strong>", "Discover <strong>yourself.</strong>"],
+            typeSpeed: 30,
+            loop: true,
+            backSpeed: 0,
+            smartBackspace: true
+        });
+
+        return () => {
+            typed.destroy();
+        };
+    }, []);
 
 
     return (
         <div className="App">
             <Header />
             <Video />
+            <hr />
+            <div className="others">
+                <span id='intro' ref={el} />
+            </div>
             <div className="container">
-                <hr />
-                <p id='intro'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint illo quae vel fugiat sit eveniet soluta omnis expedita nihil voluptatibus sequi numquam </p>
+
                 <Card title="ABOUT" text="Get all the information about the tournament;
-                the teams, basketball rules, events location, players ranking and much more!"/>
+                the teams, basketball rules, events location, players ranking and much more."/>
                 <Card title="TICKETS" text="Get your tickets now and enjoy with the 
                 street basketball family watching the best matches and 
-                having fun with a lot of activities!"/>
+                having fun with a lot of activities."/>
                 <Card title="GALLERY" text="Discover the winners from the latest editions and the
-                chronicles of every final and semifinal, It's like you were there!"/>
+                chronicles of every final and semifinal, It's like you were there."/>
                 <Card title="CONTACT" text="Feel free to ask any questions or problems, 
-                or simply provide an opinion to our support, that will help us improve!" />
+                or simply provide an opinion to our support, that will help us improve." />
             </div>
             <Footer />
         </div>
-        
+
     )
 }
 
