288bcdf05ea360bc67e33709ce5799f56ad41658
Bruno <121259934+amatbruno@users.noreply.github.com>
2023-12-15 11:51:46 +0100

    .

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,7 +4,10 @@ function Video() {
     return (
         <div className="Video">
             <img src="/src/assets/homepage_vid.gif" alt="video_motion" id='main-video' />
-            <div className="centered"><h1>THE 3X3 INTERN BARCELONA</h1></div>
+            <div className="centered">
+                <h1>THE 3X3 INTERN BARCELONA</h1>
+                <p>The new level of street basketball</p>
+            </div>
         </div>
     )
 }
