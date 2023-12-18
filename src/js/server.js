f87e8c2e4838ac6143adbd12875a7be28cc79c94
Bruno <brunoamagar@campus.monlau.com>
2023-12-18 19:15:05 +0100

    esta mierda no va

--- src/js/server.js
+++ src/js/server.js
@@ -1,4 +1,23 @@
-const express = require("express");
-const app = express();
+import express from "express";
+import pg from "pg";
 
-const PORT = process.env.PORT || 
+const app = express();
+const port = 3000;
+
+const pool = new pg.Pool({
+    host: "localhost",
+    user: "superuser",
+    password: "12345",
+    database: "3x3_inside",
+    port: 5432,
+});
+
+app.use(express.json()); // Add this line to parse JSON requests
+
+// Import routes using CommonJS syntax
+import routes from './routes.js';
+routes(app, pool);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
