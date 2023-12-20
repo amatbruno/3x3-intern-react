06b04c4b8b224e15e189a29bc2868ddfecca1d64
Bruno <brunoamagar@campus.monlau.com>
2023-12-20 16:55:54 +0100

    register 50-50

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,17 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../components/Registration.css'
 
 function Registration() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleRegister = async () => {
+        try {
+            if (password !== confirmPassword) {
+                alert("Password doesn't match");
+                return;
+            }
+
+            const response = await fetch('http://localhost:3001/api/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    password,
+                }),
+            });
+
+            if (response.ok) {
+                console.log("User registraion correct");
+            } else {
+                console.error("Error with the user registration");
+            }
+        } catch (error) {
+            console.error("Error with the user registration", error);
+        }
+    };
+
+
     return (
         <div className="sign-up-container">
             <form>
                 <h1>REGISTER</h1>
                 <span>Register with your email and password to discover all the features!</span>
-                <input type="text" placeholder='Introduce your name' />
-                <input type="email" placeholder='Introduce your email' />
-                <input type="password" placeholder='Introduce your password' />
-                <input type="password" placeholder='Confirm your password' />
-                <button>Sign In</button>
+                <input type="text" placeholder='Introduce your name' onChange={(e) => setName(e.target.value)} />
+                <input type="email" placeholder='Introduce your email' onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder='Introduce your password' onChange={(e) => setPassword(e.target.value)} />
+                <input type="password" placeholder='Confirm your password' onChange={(e) => setConfirmPassword(e.target.value)} />
+                <button type='button' onClick={handleRegister}>Sign In</button>
             </form>
         </div>
     )
---
+++ src/js/server.cjs
@@ -0,0 +1,50 @@
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require("cors");
+const app = express();
+const port = 3001;
+
+const pool = new Pool({
+    host: "localhost",
+    user: "superuser",
+    password: "12345",
+    database: "3x3_inside",
+    port: 5432,
+});
+
+app.use(express.json()); // parse JSON requests
+app.use(cors());
+
+//Register Request
+app.post('/api/register', async (req, res) => {
+    const { userId, username, realName, email, password } = req.body;
+
+    await pool.query('INSERT INTO users (user_id, username, real_name, email, password) VALUES ($1, $2, $3, $4, $5)', [userId, username, realName, email, password]) , (err, results);
+
+    if (err) {
+        console.error('Error al crear el usuario:', err);
+        res.status(500).json({ error: 'Error al crear el usuario' });
+    } else {
+        res.json({ message: 'Usuario creado con exito', admin: newAdmin });
+    }
+
+    res.json({ message: 'Usuario registrado exitosamente' });
+});
+
+//Login Request
+// app.get('/login', async (req, res) => {
+//     const { email, password } = req.body;
+
+//     // Ejemplo de consulta en la base de datos para autenticar
+//     const result = await pool.query('SELECT * FROM users WHERE email = $1 AND password = $2', [email, password]);
+
+//     if (result.rows.length > 0) {
+//         res.json({ message: 'Inicio de sesión exitoso' });
+//     } else {
+//         res.status(401).json({ message: 'Credenciales incorrectas' });
+//     }
+// });
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
