32bc887dd9f277ffe40b415971ec4adf8e557099
Bruno <brunoamagar@campus.monlau.com>
2023-12-20 16:33:05 +0100

    ,

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import './Login.css'
 import axios from 'axios';
-import { FaGoogle, FaGithub, FaLinkedin, FaFacebook } from "react-icons/fa";
 
 
 function Login() {
@@ -18,18 +17,14 @@ function Login() {
     };
 
     return (
-        <div className="container">
-            <div className="form-container sign-up">
-                <form>
-                    <h1>LOGIN</h1>
-                    <span>Enter your email and password to access your services</span>
-                    <input type="name" placeholder='Introduce your name' />
-                    <input type="text" placeholder='Introduce your email' />
-                    <input type="password" placeholder='Introduce your password' />
-                    <input type="password" placeholder='Confirm your password' />
-                    <button>Sign Up</button>
-                </form>
-            </div>
+        <div className="sign-in-container">
+            <form>
+                <h1>LOGIN</h1>
+                <span>Enter your email and password to access your services</span>
+                <input type="text" placeholder='Introduce your email' />
+                <input type="password" placeholder='Introduce your password' />
+                <button>Sign Up</button>
+            </form>
         </div>
     )
 }
--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import '../components/Registration.css'
 
 function Registration() {
     return (
-        <div className="container">
-            <div className="form-container sign-in">
-                <form>
-                    <h1>REGISTER</h1>
-                    <span>Or use your email for registration</span>
-                    <input type="text" placeholder='Introduce your email' />
-                    <input type="password" placeholder='Introduce your password' />
-                    <button>Sign In</button>
-                </form>
-            </div>
+        <div className="sign-up-container">
+            <form>
+                <h1>REGISTER</h1>
+                <span>Register with your email and password to discover all the features!</span>
+                <input type="text" placeholder='Introduce your name' />
+                <input type="email" placeholder='Introduce your email' />
+                <input type="password" placeholder='Introduce your password' />
+                <input type="password" placeholder='Confirm your password' />
+                <button>Sign In</button>
+            </form>
         </div>
     )
 }
