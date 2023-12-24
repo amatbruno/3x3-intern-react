fe65532fbab059b8ce1afce31345a2e60a354b93
Bruno <121259934+amatbruno@users.noreply.github.com>
2023-12-24 12:51:56 +0100

    mapa actualizado sin hover

--- src/views/TicketsView.jsx
+++ src/views/TicketsView.jsx
@@ -8,7 +8,7 @@ import tickets from '../data/tickets'
 function TicketsView() {
 
     const ticketsList = tickets.map((v, index) => {
-        return <Ticket key={index} date={v.date} time={v.time} category={v.category} deffered={v.deffered} team1={v.team1} team2={v.team2}/>
+        return <Ticket key={index} date={v.date} time={v.time} category={v.category} deffered={v.deffered} team1={v.team1} team2={v.team2} />
     });
 
     return (
@@ -16,9 +16,7 @@ function TicketsView() {
             <Header />
             <div className="head-elem">
                 <img id='tick-img' src="https://cdn.pixabay.com/photo/2013/02/15/02/16/basketball-81775_1280.jpg" alt="header_img" />
-                <h1>BUY YOUR TICKETS WITH A 50% OFF DISCOUNT</h1>
-                <p>This Christmas take here to your familly and/or friends to the best matches and receive especial extra discounts!</p>
-                <button className='btn-buy'>BUY NOW</button>
+                <a href='#tickt-sect' className='btn-buy'>BUY NOW</a>
             </div>
             <div className="tickets-sect" id='tickt-sect'>
                 <div className="tickets-elements">
@@ -27,6 +25,25 @@ function TicketsView() {
                 </div>
                 <div className="tickets-comp">
                     {ticketsList}
+                    <div className="zone-selection hidden">
+                        <h2>Select your sit to continue with the ticket payment:</h2>
+
+                        <div className="map-container">
+                            <img src="/src/assets/field-img.png" useMap="#image-map" />
+                        
+                        </div>
+
+                        <map name="image-map">
+                            <area target="" alt="general1" title="General 1 Zone" href="" coords="140,73,417,133" shape="rect" className="highlighted" />
+                            <area target="" alt="general2" title="General 2 Zone" href="" coords="140,326,418,385" shape="rect" />
+                            <area target="" alt="box1" title="Box 1 Zone" href="" coords="79,73,133,73,136,132,104,137,91,105" shape="poly" className="highlighted" />
+                            <area target="" alt="box2" title="Box 2 Zone" href="" coords="103,324,135,324,135,384,75,382" shape="poly" className="highlighted" />
+                            <area target="" alt="box3" title="Box 3 Zone" href="" coords="423,72,482,75,453,134,425,134" shape="poly" className="highlighted" />
+                            <area target="" alt="box4" title="Box 4 Zone" href="" coords="423,324,454,325,483,383,425,384" shape="poly" className="highlighted" />
+                            <area target="" alt="top1" title="Top 1 Zone" href="" coords="489,76,457,135,468,148,467,310,457,321,488,383,536,337,536,121" shape="poly" className="highlighted" />
+                            <area target="" alt="top2" title="Top 2 Zone" href="" coords="71,76,100,136,93,144,95,309,100,325,71,380,40,352,33,363,4,333,5,118,32,95,46,104" shape="poly" className="highlighted" />
+                        </map>
+                    </div>
                 </div>
             </div>
             <Footer />
