836ea78a1e2dd881d746f08c566d9996cc71fd42
Bruno <brunoamagar@campus.monlau.com>
2024-01-11 16:56:39 +0100

    updated user logo3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
                 <Link to="/tickets">TICKETS</Link>
                 <Link to="/gallery">GALLERY</Link>
                 <Link to="/contact">CONTACT</Link>
-                <Link to="/login"><img id='login-img' src="https://img.icons8.com/3d-fluency/94/user-male-circle.png"  alt="user-logo" /></Link>
+                <Link to="/login"><img id='login-img' src="/src/assets/login.png" alt="user-log" /></Link>
             </nav>
             <Outlet />
         </header>
