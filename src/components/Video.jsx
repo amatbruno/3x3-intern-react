225e569600ab5a3e4759adf866b097e345b69350
Bruno <brunoamagar@campus.monlau.com>
2024-01-16 19:54:16 +0100

    Homepage updates

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Video() {
     return (
         <div className="Video">
-            <img src="/src/assets/homepage_vid.gif" alt="video_motion" id='main-video' />
+            <img src="https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_1280.jpg" alt="video_motion" id='main-video' />
             <div className="centered">
                 <h1>3X3 INTERN BARCELONA</h1>
                 <p>The new level of street basketball</p>
