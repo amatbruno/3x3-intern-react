cf426fd79fc7b34b1a125fefa35420cf9086e457
Bruno <brunoamagar@campus.monlau.com>
2024-01-16 20:11:37 +0100

    Homepage updates 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,20 @@ function Header() {
     return (
         <header>
             <nav>
-                <Link to="/"><img src='/logo.png' alt="logo" /></Link>
+                <Link to="/"><img src='/src/assets/logo.png' alt="log" /></Link>
                 <Link to="/events" >EVENT</Link>
                 <Link to="/tickets">TICKETS</Link>
                 <Link to="/gallery">GALLERY</Link>
                 <Link to="/contact">CONTACT</Link>
-                <Link to="/login"><img id='login-img' src="/login.png" alt="user-log" /></Link>
+                <Link to="/login"><svg xmlns="http://www.w3.org/2000/svg"
+                    class="icon icon-tabler icon-tabler-user"
+                    width="35" height="35" viewBox="0 0 24 24"
+                    stroke-width="2" stroke="currentColor" fill="none"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                    <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                </svg></Link>
             </nav>
             <Outlet />
         </header>
